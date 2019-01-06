04565bad56b2aa79ae46e944eb5c0df1f26dcd4f
krixikraxi <absolem89@gmail.com>
2019-01-06 11:02:47 +0000

    database can now handle invoices and changed_date with trigger added

--- backupDatabase.sql
+++ backupDatabase.sql
@@ -29,10 +29,14 @@ CREATE TABLE `bills` (
   `notes` varchar(100) DEFAULT NULL,
   `user` bigint(20) NOT NULL,
   `billed` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'if the bill is already billed',
+  `invoice` bigint(20) DEFAULT NULL COMMENT 'the invoice if one already exists',
+  `change_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `bills_users_FK` (`user`),
+  KEY `bills_invoices_FK` (`invoice`),
+  CONSTRAINT `bills_invoices_FK` FOREIGN KEY (`invoice`) REFERENCES `invoices` (`id`),
   CONSTRAINT `bills_users_FK` FOREIGN KEY (`user`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,8 +45,51 @@ CREATE TABLE `bills` (
 
 LOCK TABLES `bills` WRITE;
 /*!40000 ALTER TABLE `bills` DISABLE KEYS */;
-INSERT INTO `bills` VALUES (8,50.65,'2019-01-04 09:55:51','nixiiiii',2,0),(9,14,'2019-01-04 09:56:11','spar',1,1),(10,11,'2019-01-04 10:08:01','orsch',1,0),(11,66.66,'2019-01-04 17:20:45','gaki',2,0),(13,100,'2019-01-05 16:37:07','zack',2,0),(15,20,'2019-01-05 17:02:14','franzi',1,0);
+INSERT INTO `bills` VALUES (8,50.65,'2019-01-04 09:55:51','nixiiiii',2,0,NULL,NULL),(9,14,'2019-01-04 09:56:11','spar',1,1,6,'2019-01-06 10:53:32'),(10,11,'2019-01-04 10:08:01','orsch',2,0,NULL,'2019-01-06 10:16:48'),(13,100,'2019-01-05 16:37:07','zack_fut',2,0,NULL,'2019-01-06 10:22:01'),(15,20,'2019-01-05 17:02:14','franzi',1,0,NULL,NULL),(16,130,'2019-01-06 10:02:27','irgendwas',1,0,NULL,'2019-01-06 10:18:25'),(17,12,'2019-01-06 10:35:51','furz',1,0,NULL,NULL),(18,777,'2019-01-06 10:54:15','scheisse',1,0,NULL,NULL);
 /*!40000 ALTER TABLE `bills` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`splituser`@`localhost`*/ /*!50003 TRIGGER change_date
+BEFORE UPDATE
+ON bills FOR EACH ROW
+SET NEW.change_date = NOW() */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoices` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `notes` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoices`
+--
+
+LOCK TABLES `invoices` WRITE;
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+INSERT INTO `invoices` VALUES (6,'2019-01-06 10:53:04','erste abrechnung');
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,4 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-05 17:18:09
+-- Dump completed on 2019-01-06 11:01:04
