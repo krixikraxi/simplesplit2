48d20ff35da1756a9a93925761215a59d8c860d8
krixikraxi <absolem89@gmail.com>
2019-01-11 18:36:29 +0000

    different ui improovements, success alerts, dropdown for users, update for bootstrap 4.0

--- index.php
+++ index.php
@@ -1,117 +1,173 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-<meta charset="UTF-8"> 
-<title>Splitwise 2</title>  
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>  
+	<meta charset="UTF-8"> 
+	<title>Splitwise 2</title>  
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>  
 </head>  
-<body>  
-<div class="col-md-12">
-	<h3 align="center">Splitwise 2</h3>
-	<div ng-app="sa_app" ng-controller="controller" ng-init="show_data()">
-		<div class="col-md-6">
-		   	<label>Amount</label>
-            <input type="text" name="amount" ng-model="amount" class="form-control">
-            <br/>
-            <label>Notes</label>
-            <input type="text" name="notes" ng-model="notes" class="form-control">
-            <br/>
-            <label>User</label>
-            <input type="text" name="user" ng-model="user" class="form-control">
-            <br/>
-            <input type="hidden" ng-model="id">
-            <input type="submit" name="insert" class="btn btn-primary" ng-click="insert()" value="{{btnName}}">
-		</div>
-        <div class="col-md-6">
-            <table class="table table-bordered">
-                <tr>
-                    <th>Id</th>
-                    <th>Amount</th>
-                    <th>Date</th>
-                    <th>Notes</th>
-					<th>UserName</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-                <tr ng-repeat="x in names">
-                    <td>{{x.id}}</td>
-                    <td>{{x.amount}}</td>
-                    <td>{{x.date}}</td>
-                    <td>{{x.notes}}</td>
-					<td>{{x.name}}</td>
-                    <td>
-                        <button class="btn btn-success btn-xs" ng-click="update_data(x.id, x.amount, x.notes, x.user)">
-                            <span class="glyphicon glyphicon-edit"></span> Edit
-                        </button>
-                    </td>
-                    <td>
-                        <button class="btn btn-danger btn-xs" ng-click="delete_data(x.id)">
-                            <span class="glyphicon glyphicon-trash"></span> Delete
-                        </button>
-                    </td>
-                </tr>
-            </table>
-        </div>
+<body>
+
+<div class="container">
+    <div class="page-header">
+		<h1>Splitwise 2</h1>
+		<p class="lead">This system let you split your bills between two people. </p>
     </div>
+	
+	<div id="success_alert" class="alert alert-success collapse">
+		<a id="linkClose" href="#" class="close">&times;</a>
+		<strong>Success! </strong><a id="alert_text"></a>
+	</div>
+
+	    <div ng-app="sa_app" ng-controller="controller" ng-init="show_data()">
+			<div class="row">
+				<div class="col-sm mb-4">
+				
+					<div class="input-group input-group-sm mb-3">
+					  <div class="input-group-prepend">
+						<span class="input-group-text">Amount</span>
+					  </div>
+					  <input type="text" class="form-control" ng-model="amount">
+					</div>
+					
+					<div class="input-group input-group-sm mb-3">
+					  <div class="input-group-prepend">
+						<span class="input-group-text">User</span>
+					  </div>
+						<select class="form-control form-control-sm" ng-model="user">
+							 <option ng-repeat="user in users" value='{{ user.id }}' >{{ user.name }}</option>
+						</select>
+					</div>
+					
+					<div class="input-group input-group-sm mb-3">
+					  <div class="input-group-prepend">
+						<span class="input-group-text">Notes</span>
+					  </div>
+					  <input type="text" class="form-control" ng-model="notes">
+					</div>
+
+					<input type="hidden" ng-model="id">
+					<input type="submit" name="insert" class="btn btn-primary btn-sm" ng-click="insert()" value="{{btnName}}">
+				</div>
+				<div class="col-sm">
+					Results of current data:
+					Lorem ipsum...
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm">
+					<table class="table table-bordered">
+						<tr>
+							<th>Id</th>
+							<th>Amount</th>
+							<th>Date</th>
+							<th>Notes</th>
+							<th>UserName</th>
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+						<tr ng-repeat="x in names">
+							<td>{{x.id}}</td>
+							<td>{{x.amount}}</td>
+							<td>{{x.date}}</td>
+							<td>{{x.notes}}</td>
+							<td>{{x.name}}</td>
+							<td>
+								<button class="btn btn-success btn-sm" ng-click="update_data(x.id, x.amount, x.notes, x.user)">
+									<span class="glyphicon glyphicon-edit"></span> Edit
+								</button>
+							</td>
+							<td>
+								<button class="btn btn-danger btn-sm" ng-click="delete_data(x.id)">
+									<span class="glyphicon glyphicon-trash"></span> Delete
+								</button>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+
 </div>
+
+
 <script>  
-var app = angular.module("sa_app", []);
-app.controller("controller", function($scope, $http) {
-    $scope.btnName = "Insert";
-    $scope.insert = function() {
-        if ($scope.amount == null) {
-            alert("Enter Your Amount");
-        } else if ($scope.notes == null) {
-            alert("Enter Your Notes");
-        } else if ($scope.user == null) {
-            alert("Enter Your User");
-        } else {
-            $http.post(
-                "insert.php", {
-                    'amount': $scope.amount,
-                    'notes': $scope.notes,
-                    'user': $scope.user,
-                    'btnName': $scope.btnName,
-                    'id': $scope.id
-                }
-            ).success(function(data) {
-                alert(data);
-                $scope.amount = null;
-                $scope.notes = null;
-                $scope.user = null;
-                $scope.btnName = "Insert";
-                $scope.show_data();
-            });
-        }
-    }
-    $scope.show_data = function() {
-        $http.get("display.php")
-            .success(function(data) {
-                $scope.names = data;
-            });
-    }
-    $scope.update_data = function(id, amount, notes, user) {
-        $scope.id = id;
-        $scope.amount = amount;
-        $scope.notes = notes;
-        $scope.user = user;
-        $scope.btnName = "Update";
-    }
-    $scope.delete_data = function(id) {
-        if (confirm("Are you sure you want to delete?")) {
-            $http.post("delete.php", {
-                    'id': id
-                })
-                .success(function(data) {
-                    alert(data);
-                    $scope.show_data();
-                });
-        } else {
-            return false;
-        }
-    }
-});
+	var app = angular.module("sa_app", []);
+	app.controller("controller", function($scope, $http) {
+		$scope.btnName = "Insert";
+		$scope.insert = function() {
+			if ($scope.amount == null) {
+				alert("Enter Your Amount");
+			} else if ($scope.notes == null) {
+				alert("Enter Your Notes");
+			} else if ($scope.user == null) {
+				alert("Enter Your User");
+			} else {
+				$http.post(
+					"insert.php", {
+						'amount': $scope.amount,
+						'notes': $scope.notes,
+						'user': $scope.user,
+						'btnName': $scope.btnName,
+						'id': $scope.id
+					}
+				).success(function(data) {
+					$('#success_alert').show('fade');
+					$('#alert_text').text(data);
+					setTimeout(function () {
+						$('#success_alert').hide('fade');
+					}, 5000);
+					$scope.amount = null;
+					$scope.notes = null;
+					$scope.user = null;
+					$scope.btnName = "Insert";
+					$scope.show_data();
+				});
+			}
+		}
+		$scope.show_data = function() {
+			$http.get("display.php")
+				.success(function(data) {
+					$scope.names = data;
+				});
+				
+			$http.get('user.php').
+				success(function(data) {
+					$scope.users = data;
+				});
+		}
+		$scope.update_data = function(id, amount, notes, user) {
+			$scope.id = id;
+			$scope.amount = amount;
+			$scope.notes = notes;
+			$scope.user = user;
+			$scope.btnName = "Update";
+		}
+		$scope.delete_data = function(id) {
+			if (confirm("Are you sure you want to delete?")) {
+				$http.post("delete.php", {
+						'id': id
+					})
+					.success(function(data) {
+						$('#success_alert').show('fade');
+						$('#alert_text').text(data);
+						setTimeout(function () {
+							$('#success_alert').hide('fade');
+						}, 5000);
+						$scope.show_data();
+					});
+			} else {
+				return false;
+			}
+		}
+	});
+	
+	$('#linkClose').click(function () {
+        $('#success_alert').hide('fade');
+    });
 </script>  
 </body>  
 </html>  
---
+++ user.php
@@ -0,0 +1,12 @@
+<?php
+include 'config.php';
+$output = array();
+$query  = "SELECT id, name FROM users;";
+$result = mysqli_query($conn, $query);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_array($result)) {
+        $output[] = $row;
+    }
+    echo json_encode($output);
+}
+?> 
